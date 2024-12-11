23f59141af2ec34ef756f328f0ee3747d1908c60
unknown <kalliche16@gmail.com>
2024-12-11 17:17:05 -0500

    insert authors

--- bookstore/insert_libreria.sql
+++ bookstore/insert_libreria.sql
@@ -1,10 +1,16 @@
 -- inserat registros a la tabla authors
-INSERT INTO authors (author_id, name, nationality)
-VALUES ('', 'Juan Rulfo', 'MEX');
+INSERT INTO authors (name, nationality)
+VALUES ('Juan Rulfo', 'MEX');
 
 INSERT INTO authors (name, nationality)
 VALUES ('Gabriel García Marquez', 'COL');
 
-INSERT INTO authors
-VALUES ('', 'Juan Gabriel Velazquez', 'COL');
+INSERT INTO authors (name, nationality)
+VALUES ('Juan Gabriel Velazquez', 'COL');
 
+
+INSERT INTO authors (name, nationality)
+VALUES ('Julio Cortázar', 'ARG'),
+		 ('Isabel Allende', 'CHI'),
+		 ('Octavio paz', 'MEX'),
+		 ('Juan Carlos Onetti', 'URY');
